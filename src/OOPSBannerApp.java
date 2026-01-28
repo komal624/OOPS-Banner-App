bb8ad141cd7a7882f6f64a0115cd83b9bb69dbc5
komal624 <kb4858@srmist.edu.in>
2026-01-28 23:34:55 +0530

    [Komal] Added: UC7: Store Character Pattern in a Class

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -5,19 +5,24 @@ public class OOPSBannerApp {
     }
 
     static void renderBanner() {
-        for (String line : new String[]{
-                "OOOOO   OOOOO   PPPPP   SSSSS",
-                "O   O   O   O   P   P   S",
-                "O   O   O   O   PPPPP   SSSSS",
-                "O   O   O   O   P       S",
-                "OOOOO   OOOOO   P       SSSSS"
-        }) {
+        for (String line : BannerPattern.PATTERN) {
             printLine(line);
         }
     }
 
     static void printLine(String line) {
         System.out.println(line);
+    }
+
+    // Nested static class to store banner pattern
+    static class BannerPattern {
+        static final String[] PATTERN = {
+                "OOOOO   OOOOO   PPPPP   SSSSS",
+                "O   O   O   O   P   P   S",
+                "O   O   O   O   PPPPP   SSSSS",
+                "O   O   O   O   P       S",
+                "OOOOO   OOOOO   P       SSSSS"
+        };
     }
 }
 

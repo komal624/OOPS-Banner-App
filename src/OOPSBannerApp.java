86f2fbd6f2c72e042debeb8788136aedbb6d0903
komal624 <kb4858@srmist.edu.in>
2026-01-28 23:13:09 +0530

    [Komal] Added: UC2: Render OOPS Banner using Print Statements

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,10 +1,14 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOOOO   OOOOO   PPPPP   SSSSS");
-        System.out.println("O   O   O   O   P   P   S");
-        System.out.println("O   O   O   O   PPPPP   SSSSS");
-        System.out.println("O   O   O   O   P       S");
-        System.out.println("OOOOO   OOOOO   P       SSSSS");
+
+        String banner =
+                "OOOOO   OOOOO   PPPPP   SSSSS\n" +
+                        "O   O   O   O   P   P   S\n" +
+                        "O   O   O   O   PPPPP   SSSSS\n" +
+                        "O   O   O   O   P       S\n" +
+                        "OOOOO   OOOOO   P       SSSSS";
+
+        System.out.print(banner);
     }
 }
